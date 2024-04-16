f237d4b3c3083c26e10432cd0008b3f70686a67c
wadeKeith <2470887663@qq.com>
2024-04-16 17:05:52 +0800

    nothing

--- get_expert_data.py
+++ get_expert_data.py
@@ -29,7 +29,7 @@ robot_params = {
     "reset_gripper_range": reset_gripper_range,
 }
 # control type: joint, end
-sim_params = {"use_gui":True,
+sim_params = {"use_gui":False,
             'timestep':1/240,
             'control_type':'end',
             'gripper_enable':False,
@@ -53,29 +53,28 @@ her_buffer = ReplayBuffer_Trajectory(capacity= buffer_size,
                                     achieved_goal_len=achieved_goal_len,)
 
 savetime = 0
-
 for epoch in range(10000):
     if savetime >= expert_data_num:
         break
     # reset the environment
     observation, _ = env.reset()
     traj = Trajectory(observation.copy())
-    
+    done = False
     # start to collect samples
-    step_time = 0
-    for t in range(env.time_limitation):
-        step_time += 1
+    # step_time = 0
+    while not done:
+        # step_time += 1
         # obs = observation[:state_len]
         achieved_goal = observation[state_len:state_len+achieved_goal_len]
         desired_goal = observation[state_len+achieved_goal_len:]
         # ee_pos = obs[int(state_len/2):]
         finger_pos = achieved_goal.copy()
-        print('delta',(desired_goal-finger_pos)*env.observation_space['desired_goal'].high[0])
+        # print('delta',(desired_goal-finger_pos)*env.observation_space['desired_goal'].high[0])
         dx = np.clip((desired_goal[0]-finger_pos[0])*env.observation_space['desired_goal'].high[0]/env.arm_gripper.action_scale,-1,1)
         dy = np.clip((desired_goal[1]-finger_pos[1])*env.observation_space['desired_goal'].high[0]/env.arm_gripper.action_scale,-1,1)
         dz = np.clip((desired_goal[2]-finger_pos[2])*env.observation_space['desired_goal'].high[0]/env.arm_gripper.action_scale,-1,1)
         action = np.array([dx,dy,dz])
-        print('action:',action)
+        # print('action:',action)
         observation,reward, terminated, truncated, info = env.step(action)
         done = terminated or truncated
         traj.store_step(action.copy(), observation.copy(), reward, done)
