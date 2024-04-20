892965ecbc18d7f5ffa8a4905e0541622aa584bb
wadeKeith <2470887663@qq.com>
2024-04-20 16:41:12 +0800

    nothing

---
+++ WGCSL.py
@@ -0,0 +1,200 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+import random
+from tqdm import tqdm
+import collections
+import matplotlib.pyplot as plt
+from utilize import distance
+
+
+
+class Trajectory:
+    ''' 用来记录一条完整轨迹 '''
+    def __init__(self, init_state):
+        self.states = [init_state]
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+        self.length = 0
+
+    def store_step(self, action, state, reward, done):
+        self.actions.append(action)
+        self.states.append(state)
+        self.rewards.append(reward)
+        self.dones.append(done)
+        self.length += 1
+
+
+class ReplayBuffer_Trajectory:
+    ''' 存储轨迹的经验回放池 '''
+    def __init__(self, capacity, dis_threshold, use_her, batch_size,state_len,achieved_goal_len):
+        self.buffer = collections.deque(maxlen=capacity)
+        self.dis_threshold = dis_threshold
+        self.use_her = use_her
+        self.batch_size = batch_size
+        self.state_len = state_len
+        self.achieved_goal_len = achieved_goal_len
+    def add_trajectory(self, trajectory):
+        self.buffer.append(trajectory)
+
+    def size(self):
+        return len(self.buffer)
+
+    def sample(self,her_ratio):
+        batch = dict(states=[],
+                     actions=[],
+                     next_states=[],
+                     rewards=[],
+                     dones=[])
+        for _ in range(self.batch_size):
+            traj = random.sample(self.buffer, 1)[0]
+            step_state = np.random.randint(traj.length)
+            state = traj.states[step_state]
+            next_state = traj.states[step_state + 1]
+            action = traj.actions[step_state]
+            reward = traj.rewards[step_state]
+            done = traj.dones[step_state]
+
+
+            if self.use_her and sum(traj.rewards) < 1:
+                step_goal = np.random.randint(step_state + 1, traj.length + 1)
+                goal = traj.states[step_goal][self.state_len:self.state_len+self.achieved_goal_len].copy()   # 使用HER算法的future方案设置目标
+                dis = distance(next_state[self.state_len:self.state_len+self.achieved_goal_len], goal)
+                reward = 0 if dis > self.dis_threshold else 1
+                done = False if dis > self.dis_threshold else True
+                state = np.hstack((state[:self.state_len+self.achieved_goal_len], goal)).copy() 
+                next_state = np.hstack((next_state[:self.state_len+self.achieved_goal_len], goal)).copy() 
+
+            batch['states'].append(state.copy())
+            batch['next_states'].append(next_state.copy())
+            batch['actions'].append(action.copy())
+            batch['rewards'].append(reward)
+            batch['dones'].append(done)
+
+        batch['states'] = np.array(batch['states'])
+        batch['next_states'] = np.array(batch['next_states'])
+        batch['actions'] = np.array(batch['actions'])
+        return batch
+
+
+# class PolicyNet(torch.nn.Module):
+#     def __init__(self, state_dim, hidden_dim, action_dim):
+#         super(PolicyNet, self).__init__()
+#         self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+#         self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+#         self.fc3 = torch.nn.Linear(hidden_dim, action_dim)
+
+#     def forward(self, x):
+#         x = F.relu(self.fc2(F.relu(self.fc1(x))))
+#         return torch.tanh(self.fc3(x))
+class PolicyNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(PolicyNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc_mu = torch.nn.Linear(hidden_dim, action_dim)
+        self.fc_std = torch.nn.Linear(hidden_dim, action_dim)
+        self.device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("mps")
+        )
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        mu = torch.tanh(self.fc_mu(x))
+        std = F.softplus(self.fc_std(x))
+        std = std + 1e-8 * torch.ones(size=std.shape).to(self.device)
+        return mu, std
+
+class QValueNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(QValueNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim + action_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = torch.nn.Linear(hidden_dim, 1)
+
+    def forward(self, x, a):
+        cat = torch.cat([x, a], dim=1)  # 拼接状态和动作
+        x = F.relu(self.fc2(F.relu(self.fc1(cat))))
+        return self.fc3(x)
+
+class WGCSL:
+    ''' DDPG算法 '''
+    def __init__(self, state_dim, hidden_dim, action_dim,
+                 actor_lr, critic_lr, sigma, tau, gamma, device):
+        self.action_dim = action_dim
+        self.actor = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
+        self.critic = QValueNet(state_dim, hidden_dim, action_dim).to(device)
+        self.target_critic = QValueNet(state_dim, hidden_dim,
+                                       action_dim).to(device)
+        # 初始化目标价值网络并使其参数和价值网络一样
+        self.target_critic.load_state_dict(self.critic.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),
+                                                lr=actor_lr)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),
+                                                 lr=critic_lr)
+        self.gamma = gamma
+        self.sigma = sigma  # 高斯噪声的标准差,均值直接设为0
+        self.tau = tau  # 目标网络软更新参数
+        self.device = device
+        self.state_dim = state_dim
+        self.hidden_dim = hidden_dim
+        self.epochs = 100
+        self.lr_a = actor_lr
+        self.lr_c = critic_lr
+
+    def take_action(self, state):
+        state = torch.tensor(state, dtype=torch.float).to(self.device)
+        mu, sigma = self.actor(state)
+        action_dist = torch.distributions.Normal(mu, sigma)
+        action = action_dist.sample()
+        action = action.clamp(-1.0, 1.0)
+        return action.detach().cpu().numpy()
+
+    def soft_update(self, net, target_net):
+        for param_target, param in zip(target_net.parameters(),
+                                       net.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - self.tau) +
+                                    param.data * self.tau)
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'],
+                              dtype=torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions'],
+                               dtype=torch.float).to(self.device)
+        rewards = torch.tensor(transition_dict['rewards'],
+                               dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'],
+                                   dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'],
+                             dtype=torch.float).view(-1, 1).to(self.device)
+
+        next_q_values = self.target_critic(next_states,
+                                           self.actor(next_states))
+        q_targets = rewards + self.gamma * next_q_values * (1 - dones)
+        # MSE损失函数
+        critic_loss = torch.mean(
+            F.mse_loss(self.critic(states, actions), q_targets))
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        Advantage_value = self.critic(states, self.actor(states))-self.critic(states, self.actor(next_states))
+
+        # 策略网络就是为了使Q值最大化
+        actor_loss = -torch.mean(self.critic(states, self.actor(states)))
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        self.soft_update(self.actor, self.target_actor)  # 软更新策略网络
+        self.soft_update(self.critic, self.target_critic)  # 软更新价值网络
+
+    def lr_decay(self, total_steps):
+        lr_a_now = self.lr_a * (1 - total_steps / self.epochs)
+        lr_c_now = self.lr_c * (1 - total_steps / self.epochs)
+        for p in self.actor_optimizer.param_groups:
+            p["lr"] = lr_a_now
+        for p in self.critic_optimizer.param_groups:
+            p["lr"] = lr_c_now
---
+++ tain_WGCSL.py
@@ -0,0 +1,200 @@
+import numpy as np
+from pick_place_env import PickPlace_UR5Env
+import random
+import numpy as np
+from tqdm import tqdm
+import torch
+import matplotlib.pyplot as plt
+from WGCSL import WGCSL, ReplayBuffer_Trajectory, Trajectory,PolicyNet
+import math
+import pickle
+import rl_utils
+
+def evluation_policy(env, state_dim, action_dim,hidden_dim, device, model_num):
+    model = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
+    model.load_state_dict(torch.load("./model/ddpg_her_ur5_pick_%d.pkl" % model_num))
+    model.eval()
+    episode_return = 0
+    state,_,_ = env.reset()
+    done = False
+    while not done:
+        state = torch.tensor(state, dtype=torch.float).to(device)
+        action = model(state).detach().cpu().numpy()
+        state, reward, terminated, truncated, info,_ = env.step(action)
+        done = terminated or truncated
+        episode_return += reward
+    print("Test rawrd of the model %d is %.3f and info: is_success: %r, goal is %r" % (model_num, episode_return, info['is_success'],env.goal))
+
+
+
+seed = 3407
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+reset_arm_poses = [0, -math.pi/2, math.pi*4/9, -math.pi*4/9,
+                            -math.pi/2, 0]
+reset_gripper_range = [0, 0.085]
+visual_sensor_params = {
+        'image_size': [128, 128],
+        'dist': 1.0,
+        'yaw': 90.0,
+        'pitch': -25.0,
+        'pos': [0.6, 0.0, 0.0525],
+        'fov': 75.0,
+        'near_val': 0.1,
+        'far_val': 5.0,
+        'show_vision': False
+    }
+robot_params = {
+    "reset_arm_poses": reset_arm_poses,
+    "reset_gripper_range": reset_gripper_range,
+}
+# control type: joint, end
+sim_params = {"use_gui":False,
+              'timestep':1/240,
+              'control_type':'end',
+              'gripper_enable':True,
+              'is_train':True,
+              'distance_threshold':0.05,}
+# env_kwargs_dict = {"sim_params":sim_params, "robot_params": robot_params, "visual_sensor_params": visual_sensor_params}
+
+use_expert_data = False
+
+env = PickPlace_UR5Env(sim_params, robot_params,visual_sensor_params)
+
+state_dim = env.observation_space['observation'].shape[0]+env.observation_space['desired_goal'].shape[0]+env.observation_space['achieved_goal'].shape[0]
+action_dim = env.action_space.shape[0]
+
+
+
+actor_lr = 1e-3
+critic_lr = 1e-3
+num_episodes = 100
+hidden_dim = 128
+gamma = 0.99999
+sigma = 0.5
+tau = 0.01  # 软更新参数
+buffer_size = 100000
+minimal_episodes = 5
+n_train = 5
+batch_size = 512
+state_len = env.observation_space['observation'].shape[0]
+achieved_goal_len = env.observation_space['achieved_goal'].shape[0]
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
+    "mps")
+
+
+
+
+if use_expert_data:
+    with open('ur5_pickplace_10000_expert_data.pkl', 'rb') as f:
+    # 读取并反序列化数据
+        her_buffer = pickle.load(f)
+    f.close()
+else:
+    her_buffer = ReplayBuffer_Trajectory(capacity= buffer_size, 
+                                        dis_threshold=sim_params['distance_threshold'], 
+                                        use_her=True,
+                                        batch_size=batch_size,
+                                        state_len=state_len,
+                                        achieved_goal_len=achieved_goal_len,)
+    
+agent = WGCSL(state_dim, hidden_dim, action_dim,
+                 actor_lr, critic_lr, sigma, tau, gamma, device)
+
+load_agent = False 
+agent_num = 52
+if load_agent:
+    agent.actor.load_state_dict(torch.load("./model/wgcsl_her_ur5_pick_%d.pkl" % agent_num))
+
+return_list = []
+for i in range(100):
+    agent.lr_decay(i)
+    with tqdm(total=int(num_episodes), desc='Iteration %d' % i) as pbar:
+        success_count = 0
+        for i_episode in range(num_episodes):
+            episode_return = 0
+            state,_,_ = env.reset()
+            traj = Trajectory(state.copy())
+            done = False
+            while not done:
+                with torch.no_grad():
+                    action = agent.take_action(state)
+                state, reward, terminated, truncated, info,_ = env.step(action)
+                done = terminated or truncated
+                episode_return += reward
+                traj.store_step(action.copy(), state.copy(), reward, done)
+            her_buffer.add_trajectory(traj)
+            return_list.append(episode_return)
+            if info['is_success'] == True:
+                success_count+=1
+                # her_buffer.add_trajectory(traj)
+            if her_buffer.size() >= minimal_episodes:
+                # her_buffer_len_ls = her_buffer.buffer[-1].length
+                # her_buffer_minlen_ls = [her_buffer.buffer[i].length for i in range(her_buffer.size())]
+                # her_ratio = (her_buffer_len_ls-1)/env.time_limitation
+                her_ratio = 1
+                for _ in range(n_train):
+                    transition_dict = her_buffer.sample(her_ratio)
+                    agent.update(transition_dict)
+                pbar.set_postfix({
+                    'goal':
+                    '%r' % (env.goal),
+                    # 'her_bf_min_len': min(her_buffer_minlen_ls),
+                    'her_size':her_buffer.size(),
+                    'episode':
+                        '%d' % (num_episodes* i + i_episode + 1),
+                    "her dones":np.count_nonzero(transition_dict['dones']),
+                    'return':
+                    '%.3f' % np.mean(return_list[:]),
+                    "lr": agent.actor_optimizer.param_groups[0][
+                                "lr"],
+                    "is success count": success_count,
+                    # "HER ratio":her_ratio
+                })
+            else:
+                pbar.set_postfix({
+                    'goal':
+                    '%r' % (env.goal),
+                    'episode':
+                        '%d' % (num_episodes* i + i_episode + 1),
+                    'return':
+                    '%.3f' % np.mean(return_list[:]),
+                    "lr": agent.actor_optimizer.param_groups[0][
+                                "lr"],
+                    "is success count": success_count,
+                })
+            pbar.update(1)
+    torch.save(agent.actor.state_dict(), "./model/ddpg_her_ur5_pick_%d.pkl" % i)
+    sim_params['is_train'] = False
+    # sim_params['use_gui'] = True
+    test_env  = PickPlace_UR5Env(sim_params, robot_params,visual_sensor_params)
+    evluation_policy(env=test_env, state_dim=agent.state_dim,
+                     action_dim = agent.action_dim,
+                     hidden_dim=agent.hidden_dim, 
+                     device=agent.device,
+                     model_num=i)
+    test_env.close()
+    del test_env
+    sim_params['is_train'] = True
+    # sim_params['use_gui'] = False
+
+env.close()
+del env
+with open('ddpg_her_buffer_pickplace_all.pkl', 'wb') as file:
+    pickle.dump(her_buffer, file)
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('DDPG with HER on {}'.format('UR5'))
+plt.show()
+
+mv_return = rl_utils.moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('DDPG on {}'.format('UR5'))
+plt.show()
